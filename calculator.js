be4ef8e8016dfc1d760ad1c3d4720fa1f05161d2
Kate <kateblankenship1@gmail.com>
2019-04-18 21:49:35 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,12 @@
+const x = 4;
+const firstNumber = 2;
+const secondNumber = 14;
+const thirdNumber = 20;
+const fourthNumber = 40;
+
+console.log (firstNumber + x);
+// 6
+console.log (x * thirdNumber);
+// 80
+console.log (thirdNumber - secondNumber);
+//6
