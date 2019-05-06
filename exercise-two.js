972c96594ef3a645cc30bcdff7e6a1fd748c0c89
Kate <kateblankenship1@gmail.com>
2019-05-05 19:09:18 -0700

    exercise-two created
    Ayy tequila. I mean, here is exercise two finished.

---
+++ exercise-two.js
@@ -0,0 +1,5 @@
+const sentence = 'So not drinking a homemade margarita while doing this assignment.';
+const sentenceMargarita = sentence.split(' ');
+
+console.log(sentenceMargarita);
+console.log(sentenceMargarita[2]); 
